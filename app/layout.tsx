e71270a9e821bb7c4c7e4813758a08edc9c11e0b
WisW <78122384+winsrewu@users.noreply.github.com>
2025-09-21 14:25:16 +0800

    feat: add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/globals.css"
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      url: "https://cdn.jawbts.org/photos/logo.svg",
+      type: "image/svg+xml",
+      rel: "icon"
+    }
+  ]
+}
 
 export default function RootLayout({
   children,
