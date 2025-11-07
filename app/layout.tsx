3a810821cad4ad140c5b1426903a5232000633b5
WisW <78122384+winsrewu@users.noreply.github.com>
2025-11-08 02:10:30 +0800

    style: change indent to 4 spaces

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,27 @@ import Footer from "@/components/Footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  icons: [
-    {
-      url: "https://cdn.jawbts.org/photos/logo.svg",
-      type: "image/svg+xml",
-      rel: "icon"
-    }
-  ]
+    icons: [
+        {
+            url: "https://cdn.jawbts.org/photos/logo.svg",
+            type: "image/svg+xml",
+            rel: "icon"
+        }
+    ]
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="zh" className="bg-neutral-800">
-      <body className="overflow-x-clip">
-        {children}
+    return (
+        <html lang="zh" className="bg-neutral-800">
+            <body className="overflow-x-clip">
+                {children}
 
-        <Footer />
-      </body>
-    </html>
-  );
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,46 +6,46 @@ import { Metadata } from "next";
 export const revalidate = 1800; // 30 minutes
 
 export const metadata: Metadata = {
-  title: 'Jawbts Blog',
-  description: 'Jawbts Blog',
+    title: 'Jawbts Blog',
+    description: 'Jawbts Blog',
 }
 
 export default async function Page() {
-  let blogs: UserBlog[] = [];
-  let author_avatars_base64: Map<string, string> = new Map();
-  let author_dict: Record<string, string> = {};
+    let blogs: UserBlog[] = [];
+    let author_avatars_base64: Map<string, string> = new Map();
+    let author_dict: Record<string, string> = {};
 
-  const res = await getGistBlogs();
+    const res = await getGistBlogs();
 
-  if (!res) {
-    throw new Error("Something went wrong");
-  }
-
-  blogs = res.blogs;
-  author_avatars_base64 = res.author_avatars_base64;
-
-  for (const [k, v] of author_avatars_base64) {
-    author_dict[k] = v;
-  }
-
-  // newest first
-  const sortedBlogs = blogs.sort((a, b) => {
-    if (typeof a.created_at === "string") {
-      a.created_at = new Date(a.created_at);
+    if (!res) {
+        throw new Error("Something went wrong");
     }
-    if (typeof b.created_at === "string") {
-      b.created_at = new Date(b.created_at);
+
+    blogs = res.blogs;
+    author_avatars_base64 = res.author_avatars_base64;
+
+    for (const [k, v] of author_avatars_base64) {
+        author_dict[k] = v;
     }
-    return b.created_at.getTime() - a.created_at.getTime()
-  });
 
-  return (
-    <div className="bg-neutral-800 min-h-screen text-white p-6">
-      <h1 className="text-4xl font-bold mb-8 text-left max-w-4xl mx-auto">
-        Jawbts Blogs
-      </h1>
+    // newest first
+    const sortedBlogs = blogs.sort((a, b) => {
+        if (typeof a.created_at === "string") {
+            a.created_at = new Date(a.created_at);
+        }
+        if (typeof b.created_at === "string") {
+            b.created_at = new Date(b.created_at);
+        }
+        return b.created_at.getTime() - a.created_at.getTime()
+    });
 
-      <BlogList blogs={sortedBlogs} author_avatars_base64={author_dict} />
-    </div>
-  );
+    return (
+        <div className="bg-neutral-800 min-h-screen text-white p-6">
+            <h1 className="text-4xl font-bold mb-8 text-left max-w-4xl mx-auto">
+                Jawbts Blogs
+            </h1>
+
+            <BlogList blogs={sortedBlogs} author_avatars_base64={author_dict} />
+        </div>
+    );
 }
