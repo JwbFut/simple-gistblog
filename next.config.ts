5ea1b4ef34c7ab6d09d2ef5a18d7140613cf9441
WisW <78122384+winsrewu@users.noreply.github.com>
2025-12-13 00:04:15 +0800

    feat: CDN-Cache-Control header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'CDN-Cache-Control',
+                        value: 'public, max-age=86400, stale-while-revalidate=1800', // 1 day; 30 minutes
+                    },
+                ],
+            },
+        ]
+    }
 };
 
 export default nextConfig;
